8edd68e83837fe0781a0d7234842f8b83e1a7669
Krishna009 <krish.arjun009@gmail.com>
2019-09-18 16:56:50 +0530

    main program

---
+++ marsrover/marsrover/Program.cs
@@ -0,0 +1,25 @@
+﻿using marsrover.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marsrover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DirectionContext();
+
+            var direction = context.GetDirection(Cardinals.North);
+
+            Console.WriteLine("Current direction " + direction.head);
+
+            direction = context.GetDirection(direction.head, TurnType.Left);
+
+            Console.WriteLine("After Turn " + direction.head);
+        }
+    }
+}
